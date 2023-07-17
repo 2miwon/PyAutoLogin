15729369b794ad8c12e6f2da197563c0642bd6c9
임희원 <zhengsfsf@gmail.com>
2023-07-17 23:03:43 +0900

    .

--- BoostCamp.py
+++ BoostCamp.py
@@ -1,5 +1,5 @@
 from Worker import *
-from WebAction import *
+from SeleniumAction import *
 from OutputController import *
 
 GetPage('https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=FuFWZDfBLaBvE2LUtT3Q&redirect_uri=https://www.boostcourse.org/neoid/naverCallback&state=1rqMFPk2WFiVOlJy&svctype=0')
